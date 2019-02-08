21a6c1d3895dd8da43128d6625c8207193ca48ae
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-02-08 10:05:20 -0500

    Update QueenBoard.java

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,6 +2,11 @@ public class QueenBoard{
   private int[][] board;
   public QueenBoard(int size){
     board = new int[size][size];
+    for(int r = 0; r < board.length; r++){
+      for(int c =0; c < board[0].length; c++){
+        board[r][c]=0;
+      }
+    }
   }
   private boolean addQueen(int r, int c){
     board[r][c] = 1;
@@ -21,7 +26,7 @@ public class QueenBoard{
        if(board[r][c] == 1){
         ans += "Q ";
       }
-      else{ans += "_ ";}
+      if(board[r][c] == 0){ans += "_ ";}
      }
      ans += '\n';
    }
